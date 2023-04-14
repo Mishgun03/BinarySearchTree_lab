89046584a1f174f891773f7570431edd9e3cbf46
Ivanov Mikhail <mikhailkivanov@list.ru>
2023-04-14 13:49:36 +0300

    adding normal output

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -15,20 +15,26 @@ int main() {
     bst1.insert(7);
     bst1.insert(13);
     bst1.print(std::cout);
-    std::cout << "\n" << bst1.getCount() << "\n";
+    std::cout << "\nNumber of tree nodes: " << bst1.getCount() << "\n";
+    std::cout << "iterativeInorderWalk: ";
     bst1.iterativeInorderWalk();
     std::cout << "\n";
-    std::cout << bst1.iterativeSearch(9) << "\n";
-    bst1.deleteKey(10);
+    std::cout << "Search for key 9 in the tree: " << bst1.iterativeSearch(9) << "\n";
+    std::cout << "Search for key 8 in the tree: " << bst1.iterativeSearch(8) << "\n";
+    std::cout << "Node with key 10 deleted: " << bst1.deleteKey(10) << "\n";
+    std::cout << "Node with key 9 deleted: " << bst1.deleteKey(9) << "\n";
     bst1.print(std::cout);
-    std::cout << bst1.getCount();
-    std::cout << "\n" << bst1.getHeight() << "\n";
+    std::cout << "\nNumber of tree nodes: " << bst1.getCount() << "\n";
+    std::cout << "\nTree height: " << bst1.getHeight() << "\n";
+    std::cout << "iterativeInorderWalk: ";
     bst1.iterativeInorderWalk();
     std::cout << "\n";
+    std::cout << "inorderWalk(recursively): ";
     bst1.inorderWalk();
     std::cout << "\n";
+    std::cout << "walkByLevels: ";
     bst1.walkByLevels();
-    std::cout << "\n";
+    std::cout << "\n\n";
 
     BinarySearchTree<int> bst2;
     bst2.insert(3);
@@ -37,6 +43,6 @@ int main() {
     bst2.insert(15);
     bst2.insert(0);
     bst2.print(std::cout);
-    std::cout << "\n" << bst1.isSimilar(bst2) << "\n";
-    std::cout << bst1.isIdenticalKey(bst2);
+    std::cout << "\nThe bst1 and bst2 trees are similar:" << bst1.isSimilar(bst2);
+    std::cout << "\nThe bst1 and bst2 trees contain identical keys: " << bst1.isIdenticalKey(bst2);
 }
